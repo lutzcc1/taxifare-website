d127c082d0a2459515e845cb23be285ac4faed76
Luis <lutzcc1@gmail.com>
2023-07-01 14:32:42 -0600

    use le Wagons API

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ dropoff_longitude = st.number_input("Dropoff longitude", value=40.6513111)
 dropoff_latitude = st.number_input("Dropoff latitude", value=-73.8803331)
 passenger_count = st.number_input("Passenger count", value=1, min_value=1, max_value=8, step=1)
 
-url = 'https://taxifare-prod-oainjatqlq-ew.a.run.app/predict'
+url = 'https://taxifare.lewagon.ai/predict'
 
 # Combining date and time into a single datetime object
 pickup_datetime = datetime.combine(date_and_time, time)
@@ -33,7 +33,7 @@ params = {
 response = requests.get(url, params=params)
 
 # Let's retrieve the prediction from the **JSON** returned by the API...
-prediction = response.json()['fare_amount']
+prediction = response.json()['fare']
 
 ## Finally, we can display the prediction to the user
 st.write(f"The predicted fare is: ${prediction:.2f}")
